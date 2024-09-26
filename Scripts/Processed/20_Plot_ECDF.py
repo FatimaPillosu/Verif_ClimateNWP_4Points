b0a7d36056214e799f02958dea9d4138af8f68a9
Fatima Pillosu <fatima.pillosu@ecmwf.int>
2024-09-26 16:03:43 +0000

    Modified the script that plots the CDFs. Now we plot them in real and log scale.

--- Scripts/Processed/20_Plot_ECDF.py
+++ Scripts/Processed/20_Plot_ECDF.py
@@ -2,11 +2,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import LogLocator
-from statsmodels.distributions.empirical_distribution import ECDF
 
 ###################################################################################################
 # CODE DESCRIPTION
-# 20_Plot_ECDF.py plots the ECDFs for the observational and the NWP modelled rainfall realizations.
+# 20_Plot_ECDF.py plots the obs and nwp ECDFs, real and in logarithmic scale.
 # Code runtime: ~ 2 hours.
 
 # DESCRIPTION OF INPUT PARAMETERS
@@ -14,11 +13,12 @@ from statsmodels.distributions.empirical_distribution import ECDF
 # YearF (number, in YYYY format): final year to consider.
 # Acc (number, in hours): rainfall accumulation period.
 # MinDays_Perc (float, from 0 to 1): % of min n. of days with valid obs at each location.
-# SystemNWP_list (list of string): list of NWP model climatologies.
+# SystemNWP_list (list of strings): list of NWP models.
+# SystemNWP_Colour_list (list of strings): list of colours to associate to each NWP model.
 # Git_Repo (string): path of local github repository.
-# DirIN_OBS (string): relative path for the directory containing the rainfall observational.
-# DirIN_NWP (string): relative path for the directory containing the NWP modelled rainfall realizations.
-# DirOUT (string): relative path for the directory containing the AD statistic.
+# DirIN_OBS (string): relative path for the directory containing the observed rainfall realizations.
+# DirIN_NWP (string): relative path for the directory containing the NWP rainfall realizations.
+# DirOUT (string): relative path for the directory containing the ECDFs.
 
 # INPUT PARAMETERS
 YearS = 2000
@@ -26,7 +26,7 @@ YearF = 2019
 Acc = 24
 MinDays_Perc = 0.75
 SystemNWP_list = ["Reanalysis/ERA5_EDA", "Reanalysis/ERA5", "Reforecasts/ECMWF_46r1", "Reanalysis/ERA5_ecPoint"]
-SystemNWP_Colour_list = ["brown", "limegreen", "goldenrod", "royalblue"]
+SystemNWP_Colour_list = ["#ffc001", "#1cb8a6", "#ff007f", "#2500ff"]
 Git_Repo = "/ec/vol/ecpoint_dev/mofp/Papers_2_Write/Verif_ClimateNWP_4Points"
 DirIN_OBS = "Data/Compute/10_AlignOBS_CleanSTVL/Coeff_Grid2Point_20"
 DirIN_NWP = "Data/Compute/19_Merged_tp_NWP"
@@ -34,7 +34,10 @@ DirOUT = "Data/Plot/20_ECDF"
 ###################################################################################################
 
 
-# Reading the observational climatology
+# Defining percentiles to use in the sampling of the rainfall distributions
+percs = np.arange(1,100,1)
+
+# Reading the observational rainfall realizations
 print()
 print("Reading the realizations from rainfall observations...")
 MainDirIN_OBS = Git_Repo + "/" + DirIN_OBS + "/" + f'{Acc:02d}' + "h_" + str(YearS) + "_" + str(YearF)
@@ -55,47 +58,58 @@ num_stn_MinNumDays = tp_obs_MinNumDays.shape[0]
 print(" - Total number of days between " + str(YearS) + " and " + str(YearF) + ": " + str(tp_obs.shape[1]))
 print(" - Totals number of stations with at least " + str(int(MinDays_Perc*100)) + "% of days (= " + str(int(MinNumDays)) + ") with valid observations: " + str(num_stn_MinNumDays) + "/" + str(num_stn))
 
-# Reading the NWP modelled rainfall realizations
+# Sampling the the observational rainfall distributions 
+print("Sampling the the observational rainfall distributions...")
+perc_obs = np.nanpercentile(tp_obs_MinNumDays, percs, axis = 1).T
+
+# Reading the NWP modelled rainfall realizations, and sampling them
 print()
-print("Reading the rainfall realizations from NWP models...")
-tp_nwp_all = []
+print("Reading the NWP modelled rainfall realizations, and sampling them...")
+perc_nwp_all = []
 for SystemNWP in SystemNWP_list:
       FileIN = Git_Repo + "/" + DirIN_NWP + "/MinDays_Perc_" + str(MinDays_Perc*100) + "/" + f'{Acc:02d}' + "h_" + str(YearS) + "_" + str(YearF) + "/" +  SystemNWP + "/tp.npy"
-      tp_nwp_all.append(np.load(FileIN))
-
-# Plotting the ECDFs for the rainfall distribution from observations and NWP models
+      perc_nwp_all.append(np.nanpercentile(np.load(FileIN), percs, axis = 1).T)
+      
+# Plotting the obs and nwp ECDFs (i.e. sampled rainfall distributions)
 print()
-print("Plotting the ECDFs for the rainfall distribution from observations and NWP models")
+print("Plotting the obs and nwp ECDFs (i.e. sampled rainfall distributions) for...")
 for ind_stn in range(num_stn_MinNumDays):
 
-      print(" - Creating the ECDF plots for the rain gauge n." + str(ind_stn) + "/" + str(num_stn_MinNumDays))
-      tp_obs = tp_obs_MinNumDays[ind_stn,:]
-      tp_obs_nonan = tp_obs[~np.isnan(tp_obs)] # eliminate all nan values from the calculation of the ECDF
-      ecdf_obs = ECDF(tp_obs_nonan)
+      print(" - Rain gauge n." + str(ind_stn) + "/" + str(num_stn_MinNumDays))
       
-      fig, ax = plt.subplots(figsize=(8, 6)) # initialize the figure containing the ECDFs from observations and NWP models
+      fig_real, ax_real = plt.subplots(figsize=(8, 6)) # initialize the figure containing the real ECDFs from observations and NWP models
+      fig_log, ax_log = plt.subplots(figsize=(8, 6)) # initialize the figure containing the log ECDFs from observations and NWP models
 
       for ind_nwp in range(len(SystemNWP_list)):
             
             SystemNWP = SystemNWP_list[ind_nwp]
             SystemNWP_Colour = SystemNWP_Colour_list[ind_nwp]
-            tp_nwp = tp_nwp_all[ind_nwp]
-            ecdf_nwp = ECDF(tp_nwp[ind_stn,:])
-
-            plt.plot(ecdf_nwp.x, ecdf_nwp.y * 100, marker = ".", color = SystemNWP_Colour, label = SystemNWP)
+            perc_nwp = perc_nwp_all[ind_nwp]
             
-      # Completing the plot with the ECDF from the distribution of observations
+            ax_real.plot(perc_nwp[ind_stn,:], percs, marker = ".", color = SystemNWP_Colour, label = SystemNWP)
+            ax_log.plot(perc_nwp[ind_stn,:], percs, marker = ".", color = SystemNWP_Colour, label = SystemNWP)
+
+      # Completing the nwp ECDF plots with the obs ECDF and metadata
+      ax_real.plot(perc_obs[ind_stn,:], percs, marker = ".", color = "black", label = "obs")
+      ax_real.set_xlabel("Rainfall [mm/" + str(Acc) + "h]", fontsize="14")
+      ax_real.set_ylabel("Percentiles", fontsize="14")
+      ax_real.xaxis.set_tick_params(labelsize=14)
+      ax_real.yaxis.set_tick_params(labelsize=14)
+      ax_real.grid(True, which='both', axis='y')
+      ax_real.spines['top'].set_visible(False)
+      ax_real.spines['right'].set_visible(False)
+      
       linthresh = 0.1
-      plt.plot(ecdf_obs.x, ecdf_obs.y * 100, marker = ".", color = "black", label = "obs")
-      ax.set_xlabel("Rainfall [mm/" + str(Acc) + "h]", fontsize="14")
-      ax.set_ylabel("Percentiles", fontsize="14")
-      ax.set_xscale('symlog', linthresh=linthresh, linscale=0.03)
-      ax.set_xlim([-0.1,101])
-      ax.set_ylim([0,101])
-      ax.xaxis.set_tick_params(labelsize=14)
-      ax.yaxis.set_tick_params(labelsize=14)
-      ax.xaxis.set_major_locator(plt.FixedLocator([0, 0.1, 1, 10, 100]))
-      ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda value, _: f'{value:.0f}'))
+      ax_log.plot(perc_obs[ind_stn,:], percs, marker = ".", color = "black", label = "obs")
+      ax_log.set_xlabel("Rainfall [mm/" + str(Acc) + "h]", fontsize="14")
+      ax_log.set_ylabel("Percentiles", fontsize="14")
+      ax_log.set_xscale('symlog', linthresh=linthresh, linscale=0.03)
+      ax_log.set_xlim([-0.1,101])
+      ax_log.set_ylim([0,101])
+      ax_log.xaxis.set_tick_params(labelsize=14)
+      ax_log.yaxis.set_tick_params(labelsize=14)
+      ax_log.xaxis.set_major_locator(plt.FixedLocator([0, 0.1, 1, 10, 100]))
+      ax_log.xaxis.set_major_formatter(plt.FuncFormatter(lambda value, _: f'{value:.0f}'))
       log_locator = LogLocator(base=10.0, subs=np.arange(2, 10)*0.1, numticks=100)
       def apply_log_minor_ticks(ax):
             xlim = ax.get_xlim()
@@ -105,16 +119,22 @@ for ind_stn in range(num_stn_MinNumDays):
             minor_ticks = [t for t in ax.get_xaxis().get_minor_locator().tick_values(0, 10)
                   if (t > log_start_pos or (log_start_neg is not None and t < log_start_neg))]
             ax.set_xticks(minor_ticks, minor=True)
-      apply_log_minor_ticks(ax)
+      apply_log_minor_ticks(ax_log)
+      ax_log.grid(True, which='both', axis='y')
+      ax_log.spines['top'].set_visible(False)
+      ax_log.spines['right'].set_visible(False)
 
-      ax.grid(True, which='both', axis='y')
-      ax.spines['top'].set_visible(False)
-      ax.spines['right'].set_visible(False)
+      # Saving the ECDF plots
+      MainDirOUT_real = Git_Repo + "/" + DirOUT + "/Real/MinDays_Perc_" + str(MinDays_Perc*100) + "/" + f'{Acc:02d}' + "h_" + str(YearS) + "_" + str(YearF)
+      if not os.path.exists(MainDirOUT_real):
+            os.makedirs(MainDirOUT_real)
+      FileOUT_real = "ECDF_" + str(lats_obs_MinNumDays[ind_stn]) + "_" + str(lons_obs_MinNumDays[ind_stn]) + ".png"
+      fig_real.savefig(MainDirOUT_real + "/" + FileOUT_real)
       
-      # Save the plot
-      MainDirOUT = Git_Repo + "/" + DirOUT + "/MinDays_Perc_" + str(MinDays_Perc*100) + "/" + f'{Acc:02d}' + "h_" + str(YearS) + "_" + str(YearF)
-      if not os.path.exists(MainDirOUT):
-            os.makedirs(MainDirOUT)
-      FileOUT = "ecdf_" + str(lats_obs_MinNumDays[ind_stn]) + "_" + str(lons_obs_MinNumDays[ind_stn]) + ".png"
-      fig.savefig(MainDirOUT + "/" + FileOUT)
+      MainDirOUT_log = Git_Repo + "/" + DirOUT + "/Log/MinDays_Perc_" + str(MinDays_Perc*100) + "/" + f'{Acc:02d}' + "h_" + str(YearS) + "_" + str(YearF)
+      if not os.path.exists(MainDirOUT_log):
+            os.makedirs(MainDirOUT_log)
+      FileOUT_log = "ECDF_" + str(lats_obs_MinNumDays[ind_stn]) + "_" + str(lons_obs_MinNumDays[ind_stn]) + ".png"
+      fig_log.savefig(MainDirOUT_log + "/" + FileOUT_log)
+      
       plt.close()
